01021f2ece9efc9b248b8d2156442212e36e9b21
Igor Loi <igor.loi@unibo.it>
2015-05-12 12:11:59 +0200

    Updated Level shifter with merge master

---
+++ cluster_level_shifter_in_clamp.sv
@@ -0,0 +1,11 @@
+
+module cluster_level_shifter_in_clamp
+(
+    input  logic in_i,
+    output logic out_o,
+    input  logic clamp_i
+);
+
+assign out_o = clamp_i ? 1'b0 : in_i;
+
+endmodule
---
+++ cluster_level_shifter_in_clamp_cmos028fdsoi.sv
@@ -0,0 +1,31 @@
+
+// C12T32_LLN2U4S_BFX16
+
+`include "ulpsoc_defines.sv"
+
+module cluster_level_shifter_in
+(
+    input  logic        in_i,
+    output logic        out_o,
+    input  logic        clamp_i
+);
+
+`ifdef CMOS28FDSOI_8T
+    C8T28SOIDV_LRV_LSINCL0X32 lsin
+    (
+        .Z(out_o),
+        .A(in_i),
+        .R(clamp_i)
+    );
+`endif
+
+
+`ifdef CMOS28FDSOI_12T_UWVR
+    C12T32_LLN2U4S_BFX16 lsin
+    (
+        .Z(out_o),
+        .A(in_i)
+    );
+`endif 
+
+endmodule
