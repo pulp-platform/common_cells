3c0e3faa5776b29f798bd6bd304260c187af4759
Igor Loi <igor.loi@unibo.it>
2015-05-12 11:30:28 +0200

    Merge branch 'master' of iis-codendi.ee.ethz.ch:/gitroot/pulp/PULP
    Conflicts:
    	pulp3/fe/rtl/components/pad_frame_mia.sv
    	pulp3/fe/rtl/ulpsoc/pulp_chip.sv

--- pulp_level_shifter_in_cmos028fdsoi.sv
+++ pulp_level_shifter_in_cmos028fdsoi.sv
@@ -4,17 +4,19 @@
 `include "ulpsoc_defines.sv"
 
 module pulp_level_shifter_in
-  (
-   input  logic in_i,
-   output logic out_o
-   );
+(
+    input  logic        in_i,
+    output logic        out_o
+);
 
 
 
 `ifdef CMOS28FDSOI_8T
-
-    assign out_o = in_i;
-   
+    C8T28SOIDV_LRV_LSINX32 lsin
+    (
+        .Z(out_o),
+        .A(in_i)
+    );
 `endif
 
 
--- pulp_level_shifter_out_cmos028fdsoi.sv
+++ pulp_level_shifter_out_cmos028fdsoi.sv
@@ -4,26 +4,27 @@
 `include "ulpsoc_defines.sv"
 
 module pulp_level_shifter_out
-  (
-   input  logic in_i,
-   output logic out_o
-   );
+(
+    input  logic in_i,
+    output logic out_o
+);
 
 
 `ifdef CMOS28FDSOI_8T
-
-    assign out_o = in_i;
-   
+    C8T28SOITV_LRV_LSOUTX32 lsout
+    (
+        .Z(out_o),
+        .A(in_i)
+    )
 `endif
 
 
 `ifdef CMOS28FDSOI_12T_UWVR
-   C12T32DG_LLU_LSINX26 
-     lsout
-       (
-	.Z(out_o), 
-	.A(in_i)
-	);
+    C12T32DG_LLU_LSINX26   lsout
+    (
+        .Z(out_o),
+        .A(in_i)
+    );
 `endif 
 
 
