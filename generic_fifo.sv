300647411610c52318917f1dc120581013a64864
Igor Loi <igor.loi@unibo.it>
2015-09-02 14:52:42 +0200

    updated generic_fifo

--- generic_fifo.sv
+++ generic_fifo.sv
@@ -32,6 +32,7 @@
 //                                                                               //
 // Revision:                                                                     //
 // Revision v0.1 - 01/02/2014 : File Created                                     //
+// Revision v0.2 - 02/09/2015 : Updated with a global CG cell                    //
 //                                                                               //
 //                                                                               //
 //                                                                               //
@@ -42,123 +43,92 @@
 
 module generic_fifo 
 #( 
-  parameter       DATA_WIDTH = 32,
-  parameter       DATA_DEPTH = 8
+   parameter                       DATA_WIDTH = 32,
+   parameter                       DATA_DEPTH = 8
 )
-
 (
-  input  logic                    clk,
-  input  logic                    rst_n,
-  //PUSH SIDE
-  input  logic [DATA_WIDTH-1:0]   DATA_IN,
-  input  logic                    VALID_IN,
-  output logic                    GRANT_OUT,
-  //POP SIDE
-  output logic [DATA_WIDTH-1:0]   DATA_OUT,
-  output logic                    VALID_OUT,
-  input  logic                    GRANT_IN,
+   input  logic                                    clk,
+   input  logic                                    rst_n,
+   //PUSH SIDE
+   input  logic [DATA_WIDTH-1:0]                   DATA_IN,
+   input  logic                                    VALID_IN,
+   output logic                                    GRANT_OUT,
+   //POP SIDE
+   output logic [DATA_WIDTH-1:0]                   DATA_OUT,
+   output logic                                    VALID_OUT,
+   input  logic                                    GRANT_IN,
 
-  input  logic                    test_en_i
+   input  logic                                    test_en_i
 );
 
 
-  // Local Parameter
-  localparam        ADDR_DEPTH = $clog2(DATA_DEPTH);
-  enum logic [1:0] { EMPTY, FULL, MIDDLE } CS, NS;
-  // Internal Signals
+   // Local Parameter
+   localparam  ADDR_DEPTH = $clog2(DATA_DEPTH);
+   enum logic [1:0] { EMPTY, FULL, MIDDLE } CS, NS;
+   // Internal Signals
 
-  logic                     en_clk;
-  logic                     clk_gated;
+   logic       gate_clock;
+   logic       clk_gated;
 
-  logic  [DATA_DEPTH-1:0]   clk_buffer_slot;
-  logic  [DATA_DEPTH-1:0]   en_slot_clock;
-
-  logic [ADDR_DEPTH-1:0]    Pop_Pointer_CS,  Pop_Pointer_NS;
-  logic [ADDR_DEPTH-1:0]    Push_Pointer_CS, Push_Pointer_NS;
-
-  logic [DATA_WIDTH-1:0]    FIFO_REGISTERS[DATA_DEPTH-1:0];
-
-  genvar i,j;
+   logic [ADDR_DEPTH-1:0]          Pop_Pointer_CS,  Pop_Pointer_NS;
+   logic [ADDR_DEPTH-1:0]          Push_Pointer_CS, Push_Pointer_NS;
+   logic [DATA_WIDTH-1:0]          FIFO_REGISTERS[DATA_DEPTH-1:0];
+   integer                         i;
 
 
 
-  // Parameter Check
-  // synopsys translate_off
-  initial
-  begin : parameter_check
-    integer param_err_flg;
-    param_err_flg = 0;
-    
-    if (DATA_WIDTH < 1)
-    begin
-      param_err_flg = 1;
-      $display("ERROR: %m :\n  Invalid value (%d) for parameter DATA_WIDTH (legal range: greater than 1)", DATA_WIDTH );
-    end
+   // Parameter Check
+   // synopsys translate_off
+   initial
+   begin : parameter_check
+      integer param_err_flg;
+      param_err_flg = 0;
 
-    if (DATA_DEPTH < 1)
-    begin
-      param_err_flg = 1;
-      $display("ERROR: %m :\n  Invalid value (%d) for parameter DATA_DEPTH (legal range: greater than 1)", DATA_DEPTH );
-    end         
-  end
-  // synopsys translate_on
+      if (DATA_WIDTH < 1)
+      begin
+         param_err_flg = 1;
+         $display("ERROR: %m :\n  Invalid value (%d) for parameter DATA_WIDTH (legal range: greater than 1)", DATA_WIDTH );
+      end
+
+      if (DATA_DEPTH < 1)
+      begin
+         param_err_flg = 1;
+         $display("ERROR: %m :\n  Invalid value (%d) for parameter DATA_DEPTH (legal range: greater than 1)", DATA_DEPTH );
+      end                   
+   end
+   // synopsys translate_on
 
 
-  cluster_clock_gating top_cg_cell
-  (
+   cluster_clock_gating cg_cell
+   (
      .clk_i     ( clk        ),
-     .en_i      ( en_clk     ),
+     .en_i      (~gate_clock ),
      .test_en_i ( test_en_i  ),
      .clk_o     ( clk_gated  )
    );
 
-
-  always_comb 
-  begin
-    en_slot_clock = '0;
-    en_slot_clock[Push_Pointer_CS] = ((VALID_IN == 1'b1) && ( CS != FULL));
-  end
-
-
-
-
-  generate
-  for (i = 0; i < DATA_DEPTH; i++) 
-  begin
-      cluster_clock_gating single_entry_cg_cell
-      (
-         .clk_i     ( clk_gated            ),
-         .en_i      ( en_slot_clock[i]     ),
-         .test_en_i ( test_en_i            ),
-         .clk_o     ( clk_buffer_slot[i]   )
-       );
-  end
-  endgenerate
+   // UPDATE THE STATE
+   always_ff @(posedge clk, negedge rst_n)
+   begin
+       if(rst_n == 1'b0)
+       begin
+               CS              <= EMPTY;
+               Pop_Pointer_CS  <= {ADDR_DEPTH {1'b0}};
+               Push_Pointer_CS <= {ADDR_DEPTH {1'b0}};
+       end
+       else
+       begin
+               CS              <= NS;
+               Pop_Pointer_CS  <= Pop_Pointer_NS;
+               Push_Pointer_CS <= Push_Pointer_NS;
+       end
+   end
 
 
-
-  // UPDATE THE STATE
-  always_ff @(posedge clk, negedge rst_n)
-  begin
-    if(rst_n == 1'b0)
-    begin
-      CS              <= EMPTY;
-      Pop_Pointer_CS  <= {ADDR_DEPTH {1'b0}};
-      Push_Pointer_CS <= {ADDR_DEPTH {1'b0}};
-    end
-    else
-    begin
-      CS              <= NS;
-      Pop_Pointer_CS  <= Pop_Pointer_NS;
-      Push_Pointer_CS <= Push_Pointer_NS;
-    end
-  end
-
-
-  // Compute Next State
-  always_comb
-  begin
-      en_clk      = 1'b1;
+   // Compute Next State
+   always_comb
+   begin
+      gate_clock      = 1'b0;
 
       case(CS)
 
@@ -170,17 +140,17 @@ module generic_fifo
           case(VALID_IN)
           1'b0 : 
           begin 
-            NS              = EMPTY;
-            Push_Pointer_NS = Push_Pointer_CS;
-            Pop_Pointer_NS  = Pop_Pointer_CS;
-            en_clk          = 1'b1; 
+                  NS                      = EMPTY;
+                  Push_Pointer_NS = Push_Pointer_CS;
+                  Pop_Pointer_NS  = Pop_Pointer_CS;
+                  gate_clock      = 1'b1;
           end
 
           1'b1: 
           begin 
-            NS              = MIDDLE;
-            Push_Pointer_NS = Push_Pointer_CS + 1'b1;
-            Pop_Pointer_NS  = Pop_Pointer_CS;
+                  NS                      = MIDDLE;
+                  Push_Pointer_NS = Push_Pointer_CS + 1'b1;
+                  Pop_Pointer_NS  = Pop_Pointer_CS;
           end
 
           endcase
@@ -195,94 +165,94 @@ module generic_fifo
 
           2'b01:
           begin
-            en_clk      = 1'b0;
+                  gate_clock      = 1'b1;
 
-            if((Pop_Pointer_CS == Push_Pointer_CS -1 ) || ((Pop_Pointer_CS == DATA_DEPTH-1) && (Push_Pointer_CS == 0) ))
-              NS      = EMPTY;
-            else
-              NS      = MIDDLE;
+                  if((Pop_Pointer_CS == Push_Pointer_CS -1 ) || ((Pop_Pointer_CS == DATA_DEPTH-1) && (Push_Pointer_CS == 0) ))
+                          NS              = EMPTY;
+                  else
+                          NS              = MIDDLE;
 
-            Push_Pointer_NS = Push_Pointer_CS;
+                  Push_Pointer_NS = Push_Pointer_CS;
 
-            if(Pop_Pointer_CS == DATA_DEPTH-1)
-              Pop_Pointer_NS  = 0;
-            else
-              Pop_Pointer_NS  = Pop_Pointer_CS + 1'b1;
+                  if(Pop_Pointer_CS == DATA_DEPTH-1)
+                          Pop_Pointer_NS  = 0;
+                  else
+                          Pop_Pointer_NS  = Pop_Pointer_CS + 1'b1;
           end
 
           2'b00 : 
           begin
-            en_clk          = 1'b0; 
-            NS              = MIDDLE;
-            Push_Pointer_NS = Push_Pointer_CS;
-            Pop_Pointer_NS  = Pop_Pointer_CS;
+                  gate_clock      = 1'b1; 
+                  NS                      = MIDDLE;
+                  Push_Pointer_NS = Push_Pointer_CS;
+                  Pop_Pointer_NS  = Pop_Pointer_CS;
           end
 
           2'b11: 
           begin
-            NS      = MIDDLE;
+                  NS              = MIDDLE;
 
-            if(Push_Pointer_CS == DATA_DEPTH-1)
-              Push_Pointer_NS = 0;
-            else
-              Push_Pointer_NS = Push_Pointer_CS + 1'b1;
+                  if(Push_Pointer_CS == DATA_DEPTH-1)
+                          Push_Pointer_NS = 0;
+                  else
+                          Push_Pointer_NS = Push_Pointer_CS + 1'b1;
 
-            if(Pop_Pointer_CS == DATA_DEPTH-1)
-              Pop_Pointer_NS  = 0;
-            else
-              Pop_Pointer_NS  = Pop_Pointer_CS  + 1'b1;
+                  if(Pop_Pointer_CS == DATA_DEPTH-1)
+                          Pop_Pointer_NS  = 0;
+                  else
+                          Pop_Pointer_NS  = Pop_Pointer_CS  + 1'b1;
           end
 
           2'b10:
           begin 
-            if(( Push_Pointer_CS == Pop_Pointer_CS - 1) || ( (Push_Pointer_CS == DATA_DEPTH-1) && (Pop_Pointer_CS == 0) ))
-              NS      = FULL;
-            else
-              NS    = MIDDLE;
+                  if(( Push_Pointer_CS == Pop_Pointer_CS - 1) || ( (Push_Pointer_CS == DATA_DEPTH-1) && (Pop_Pointer_CS == 0) ))
+                          NS              = FULL;
+                  else
+                          NS        = MIDDLE;
 
-            if(Push_Pointer_CS == DATA_DEPTH - 1)
-              Push_Pointer_NS = 0;
-            else
-              Push_Pointer_NS = Push_Pointer_CS + 1'b1;
+                  if(Push_Pointer_CS == DATA_DEPTH - 1)
+                          Push_Pointer_NS = 0;
+                  else
+                          Push_Pointer_NS = Push_Pointer_CS + 1'b1;
 
-            Pop_Pointer_NS  = Pop_Pointer_CS;
+                  Pop_Pointer_NS  = Pop_Pointer_CS;
           end
 
-          endcase         
+          endcase                     
       end
 
       FULL:
       begin
           GRANT_OUT = 1'b0;
           VALID_OUT = 1'b1;
-          en_clk    = 1'b0;
+          gate_clock      = 1'b1;
 
           case(GRANT_IN)
           1'b1: 
           begin 
-            NS      = MIDDLE;
+                  NS              = MIDDLE;
 
-            Push_Pointer_NS = Push_Pointer_CS;
+                  Push_Pointer_NS = Push_Pointer_CS;
 
-            if(Pop_Pointer_CS == DATA_DEPTH-1)
-              Pop_Pointer_NS  = 0;
-            else
-              Pop_Pointer_NS  = Pop_Pointer_CS  + 1'b1;
+                  if(Pop_Pointer_CS == DATA_DEPTH-1)
+                          Pop_Pointer_NS  = 0;
+                  else
+                          Pop_Pointer_NS  = Pop_Pointer_CS  + 1'b1;
           end
 
           1'b0:
           begin 
-            NS      = FULL;
-            Push_Pointer_NS = Push_Pointer_CS;
-            Pop_Pointer_NS  = Pop_Pointer_CS;
+                  NS              = FULL;
+                  Push_Pointer_NS = Push_Pointer_CS;
+                  Pop_Pointer_NS  = Pop_Pointer_CS;
           end
-          endcase     
+          endcase                 
 
       end // end of FULL
 
       default :
       begin
-          en_clk          = 1'b0;
+          gate_clock      = 1'b1;
           GRANT_OUT       = 1'b0;
           VALID_OUT       = 1'b0;
           NS              = EMPTY;
@@ -291,26 +261,22 @@ module generic_fifo
       end
 
       endcase
-  end
+   end
 
-
-  generate
-  for ( j = 0; j < DATA_DEPTH; j++ ) 
-  begin
-      always_ff @(posedge clk_buffer_slot[j], negedge rst_n)
+   always_ff @(posedge clk_gated, negedge rst_n)
+   begin
+      if(rst_n == 1'b0)
       begin
-          if(rst_n == 1'b0)
-                FIFO_REGISTERS[j] <= {DATA_WIDTH {1'b0}};
-          else
-                FIFO_REGISTERS[j] <= DATA_IN;
+      for (i=0; i< DATA_DEPTH; i++)
+         FIFO_REGISTERS[i] <= {DATA_WIDTH {1'b0}};
       end
-  end
-  endgenerate
+      else
+      begin
+         if((GRANT_OUT == 1'b1) && (VALID_IN == 1'b1))
+            FIFO_REGISTERS[Push_Pointer_CS] <= DATA_IN;
+      end
+   end
 
+   assign DATA_OUT = FIFO_REGISTERS[Pop_Pointer_CS];
 
-  assign DATA_OUT = FIFO_REGISTERS[Pop_Pointer_CS];
-
-
-
-
-endmodule
+endmodule // generic_fifo
