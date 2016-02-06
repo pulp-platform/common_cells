b919be1f14d3570581faa16b09f2df473b90f783
Francesco Conti <f.conti@unibo.it>
2016-02-06 23:55:31 +0100

    added cluster_clock_gating bypass for FPGA

---
+++ cluster_clock_gating_xilinx.sv
@@ -0,0 +1,11 @@
+module cluster_clock_gating
+  (
+   input  logic clk_i,
+   input  logic en_i,
+   input  logic test_en_i,
+   output logic clk_o
+   );
+
+   assign clk_o = clk_i;
+   
+endmodule
