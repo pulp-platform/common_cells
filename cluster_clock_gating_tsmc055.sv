2478cacd667f0b603da584d814da346ad1ff0276
Igor Loi <igor.loi@unibo.it>
2016-06-14 19:31:51 +0200

    added techno dep cells for TSMC 55nm lib

---
+++ cluster_clock_gating_tsmc055.sv
@@ -0,0 +1,38 @@
+// Regular VT
+// LAGCEPM2R, LAGCEPM3R, LAGCEPM4R
+// LAGCEPM6R, LAGCEPM8R
+// LAGCEPM12R, LAGCEPM16R
+// LAGCEPM20R
+
+// Low VT
+// LAGCEPM2W, LAGCEPM3W, LAGCEPM4W
+// LAGCEPM6W, LAGCEPM8W
+// LAGCEPM12W, LAGCEPM16W
+// LAGCEPM20W
+
+// High VT
+// LAGCEPM2S, LAGCEPM3S, LAGCEPM4S
+// LAGCEPM6S, LAGCEPM8S
+// LAGCEPM12S, LAGCEPM16S
+// LAGCEPM20S
+
+`include "ulpsoc_defines.sv"
+
+module cluster_clock_gating
+(
+    input  logic clk_i,
+    input  logic en_i,
+    input  logic test_en_i,
+    output logic clk_o
+);
+
+    //LAGCEPM12R  clk_gate_i
+    TLATNTSCA_X12_A8TR clk_gate_i
+    (
+        .ECK(clk_o),
+        .CK(clk_i),
+        .E(en_i),
+        .SE(test_en_i)
+    );
+
+endmodule
---
+++ cluster_clock_xor2_tsmc055.sv
@@ -0,0 +1,29 @@
+// Regular VT
+// CKXOR2M1RA, CKXOR2M2RA, CKXOR2M4RA
+// CKXOR2M8RA, CKXOR2M12RA
+
+// Low VT
+// CKXOR2M1WA, CKXOR2M2WA, CKXOR2M4WA
+// CKXOR2M8WA, CKXOR2M12WA
+
+// High VT
+// CKXOR2M1SA, CKXOR2M2SA, CKXOR2M4SA
+// CKXOR2M8SA, CKXOR2M12SA
+
+module cluster_clock_xor2
+(
+   input  logic clk0_i,
+   input  logic clk1_i,
+   output logic clk_o
+);
+
+
+   CLKXOR2_X12_A8TR clk_xor_i 
+   (
+      .Y(clk_o),
+      .A(clk0_i),
+      .B(clk1_i) 
+   );
+
+
+endmodule
