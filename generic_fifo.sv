5c586d844aeea819b8b97d6b17fbec9d305ab4a6
Stefan Mach <smach@iis.ee.ethz.ch>
2017-04-06 18:47:33 +0200

    Added proprietary license header to all source files.

--- generic_fifo.sv
+++ generic_fifo.sv
@@ -1,13 +1,20 @@
-// ============================================================================= //
-//                           COPYRIGHT NOTICE                                    //
-// Copyright 2014 Multitherman Laboratory - University of Bologna                //
-// ALL RIGHTS RESERVED                                                           //
-// This confidential and proprietary software may be used only as authorised by  //
-// a licensing agreement from Multitherman Laboratory - University of Bologna.   //
-// The entire notice above must be reproduced on all authorized copies and       //
-// copies may only be made to the extent permitted by a licensing agreement from //
-// Multitherman Laboratory - University of Bologna.                              //
-// ============================================================================= //
+// ========================================================================== //
+//                           COPYRIGHT NOTICE                                 //
+// Copyright (C) 2017 ETH Zurich, University of Bologna                       //
+// All rights reserved.                                                       //
+//                                                                            //
+// This code is under development and not yet released to the public.         //
+// Until it is released, the code is under the copyright of ETH Zurich and    //
+// the University of Bologna, and may contain confidential and/or unpublished //
+// work. Any reuse/redistribution is strictly forbidden without written       //
+// permission from ETH Zurich.                                                //
+//                                                                            //
+// Bug fixes and contributions will eventually be released under the          //
+// SolderPad open hardware license in the context of the PULP platform        //
+// (http://www.pulp-platform.org), under the copyright of ETH Zurich and the  //
+// University of Bologna.                                                     //
+//                                                                            //
+// ========================================================================== //
 
 // ============================================================================= //
 // Company:        Multitherman Laboratory @ DEIS - University of Bologna        //
--- pulp_clock_xor2_umc065.sv
+++ pulp_clock_xor2_umc065.sv
@@ -1,3 +1,18 @@
+/* Copyright (C) 2017 ETH Zurich, University of Bologna
+ * All rights reserved.
+ *
+ * This code is under development and not yet released to the public.
+ * Until it is released, the code is under the copyright of ETH Zurich and
+ * the University of Bologna, and may contain confidential and/or unpublished 
+ * work. Any reuse/redistribution is strictly forbidden without written
+ * permission from ETH Zurich.
+ *
+ * Bug fixes and contributions will eventually be released under the
+ * SolderPad open hardware license in the context of the PULP platform
+ * (http://www.pulp-platform.org), under the copyright of ETH Zurich and the
+ * University of Bologna.
+ */
+
 // Regular VT
 // CKXOR2M1RA, CKXOR2M2RA, CKXOR2M4RA
 // CKXOR2M8RA, CKXOR2M12RA
