30b67bc9d79791120aef25f74e236176f3ae82e1
Michael Schaffner <schaffner@iis.ee.ethz.ch>
2018-10-25 19:21:09 +0200

    Fix multiply driven nets in case of 2 requesters and enabled lock feature.

--- src/rrarbiter.sv
+++ src/rrarbiter.sv
@@ -21,8 +21,8 @@
 //
 // the arbiter has a request signal vector input (req_i) and an ack
 // signal vector ouput (ack_o). to enable the arbiter the signal
-// en_i has to be asserted. vld_o is high when one of the
-// req_i signals is acknowledged.
+// en_i has to be asserted. vld_o is high when one of the req_i signals is
+// acknowledged.
 //
 // the entity has one register which stores the index of the last request signal
 // that was served.
@@ -32,12 +32,11 @@
 // arbitration will be locked until en_i is asserted again.
 //
 // dependencies: relies on fast leading zero counter tree "lzc" in common_cells
-//
 
 module rrarbiter #(
-    parameter NUM_REQ = 13,
-    parameter LOCK_IN = 0
-)(
+    parameter unsigned int NUM_REQ = 13,
+    parameter unsigned int LOCK_IN = 0
+) (
     input logic                         clk_i,
     input logic                         rst_ni,
 
@@ -46,7 +45,7 @@ module rrarbiter #(
     input logic [NUM_REQ-1:0]           req_i,   // request signals
 
     output logic [NUM_REQ-1:0]          ack_o,   // acknowledge signals
-    output logic                        vld_o,   // valid signals
+    output logic                        vld_o,   // request ack'ed
     output logic [$clog2(NUM_REQ)-1:0]  idx_o    // idx output
     );
 
@@ -71,93 +70,91 @@ logic lock_d, lock_q;
 assign idx_o          = arb_sel_d;
 assign vld_o          = (|req_i) & en_i;
 
-generate
-    if(LOCK_IN > 0) begin
-        // latch decision in case we got at least one req and no acknowledge
-        assign lock_d         = (|req_i) & ~en_i;
-        assign arb_sel_lock_d = arb_sel_d;
-    end else begin
-        // disable
-        assign lock_d         = '0;
-        assign arb_sel_lock_d = '0;
-    end
-endgenerate
 
-// only 2 input requesters
-generate
-    if (NUM_REQ == 2) begin
-            assign arb_sel_d = (( arb_sel_q) | (~arb_sel_q & ~req_i[0])) & req_i[1];
-            assign ack_o[0]  = ((~arb_sel_q) | ( arb_sel_q & ~req_i[1])) & req_i[0] & en_i;
-            assign ack_o[1]  = arb_sel_d                                            & en_i;
-    end
-endgenerate
-
-// more than 2 requesters
-generate
-    if (NUM_REQ > 2 || LOCK_IN) begin
-        // this mask is used to mask the incoming req vector
-        // depending on the index of the last served index
-        assign mask = mask_lut[arb_sel_q];
-
-        // get index from masked vectors
-        lzc #(
-            .WIDTH(NUM_REQ)
-        ) i_lower_ff1 (
-            .in_i    ( masked_lower  ),
-            .cnt_o   ( lower_idx     ),
-            .empty_o ( no_lower_ones )
-        );
-
-        lzc #(
-            .WIDTH(NUM_REQ)
-        ) i_upper_ff1 (
-            .in_i    ( masked_upper  ),
-            .cnt_o   ( upper_idx     ),
-            .empty_o (               )
-        );
-
-        // wrap around
-        assign next_idx   = (no_lower_ones)      ? upper_idx : lower_idx;
-        assign arb_sel_d  = (lock_q)             ? arb_sel_lock_q :
-                            (next_idx < NUM_REQ) ? next_idx       : unsigned'(NUM_REQ-1);
-    end
-endgenerate
-
-generate
-    for (genvar k=0; (k < NUM_REQ) && NUM_REQ > 2; k++) begin
-        assign mask_lut[k]     = unsigned'(2**(k+1)-1);
-        assign masked_lower[k] = (~ mask[k]) & req_i[k];
-        assign masked_upper[k] = mask[k]     & req_i[k];
-        assign ack_o[k]        = ((arb_sel_d == k) & vld_o ) ? 1'b1 : 1'b0;
-    end
-endgenerate
-
-// regs
-always_ff @(posedge clk_i or negedge rst_ni) begin : p_regs
-    if(~rst_ni) begin
-        arb_sel_q      <= 0;
-        lock_q         <= 0;
-        arb_sel_lock_q <= 0;
-    end else begin
-        lock_q         <= lock_d;
-        arb_sel_lock_q <= arb_sel_lock_d;
-        if (vld_o) begin
-            arb_sel_q      <= arb_sel_d;
-        end
-        if (flush_i) begin
-            arb_sel_q      <= 0;
-            lock_q         <= 0;
-            arb_sel_lock_q <= 0;
-        end
-    end
+if (LOCK_IN > 0) begin : g_lock_in
+    // latch decision in case we got at least one req and no acknowledge
+    assign lock_d         = (|req_i) & ~en_i;
+    assign arb_sel_lock_d = arb_sel_d;
+end else begin
+    // disable
+    assign lock_d         = '0;
+    assign arb_sel_lock_d = '0;
 end
 
+// only 2 input requesters
+if (NUM_REQ == 2 && !LOCK_IN) begin : g_rrlogic
+
+    assign arb_sel_d = (( arb_sel_q) | (~arb_sel_q & ~req_i[0])) & req_i[1];
+    assign ack_o[0]  = ((~arb_sel_q) | ( arb_sel_q & ~req_i[1])) & req_i[0] & en_i;
+    assign ack_o[1]  = arb_sel_d                                            & en_i;
+
+end else begin
+
+    // this mask is used to mask the incoming req vector
+    // depending on the index of the last served index
+    assign mask = mask_lut[arb_sel_q];
+
+    // get index from masked vectors
+    lzc #(
+        .WIDTH ( NUM_REQ )
+    ) i_lower_ff1 (
+        .in_i    ( masked_lower  ),
+        .cnt_o   ( lower_idx     ),
+        .empty_o ( no_lower_ones )
+    );
+
+    lzc #(
+        .WIDTH ( NUM_REQ )
+    ) i_upper_ff1 (
+        .in_i    ( masked_upper  ),
+        .cnt_o   ( upper_idx     ),
+        .empty_o (               )
+    );
+
+    // wrap around
+    assign next_idx   = (no_lower_ones)      ? upper_idx      :
+                                               lower_idx;
+    assign arb_sel_d  = (lock_q)             ? arb_sel_lock_q :
+                        (next_idx < NUM_REQ) ? next_idx       :
+                                               unsigned'(NUM_REQ-1);
+end
+
+
+
+for (genvar k=0; (k < NUM_REQ) && (NUM_REQ > 2 || LOCK_IN); k++) begin : g_mask
+    assign mask_lut[k]     = unsigned'(2**(k+1)-1);
+    assign masked_lower[k] = (~mask[k]) & req_i[k];
+    assign masked_upper[k] = mask[k]    & req_i[k];
+    assign ack_o[k]        = ((arb_sel_d == k) && vld_o );
+end
+
+
+always_ff @(posedge clk_i or negedge rst_ni) begin : p_regs
+    if(~rst_ni) begin
+        arb_sel_q      <= '0;
+        lock_q         <= 1'b0;
+        arb_sel_lock_q <= '0;
+    end else begin
+        if (flush_i) begin
+            arb_sel_q      <= '0;
+            lock_q         <= 1'b0;
+            arb_sel_lock_q <= '0;
+        end else begin
+            lock_q         <= lock_d;
+            arb_sel_lock_q <= arb_sel_lock_d;
+            if (vld_o) begin
+                arb_sel_q  <= arb_sel_d;
+            end
+        end
+
+    end
+end
 
 // pragma translate_off
 `ifndef VERILATOR
     // check parameterization, enable and hot1 property of acks
     // todo: check RR fairness with sequence assertion
-    initial begin
+    initial begin : p_assertions
         assert (NUM_REQ>=2) else $fatal ("minimum input width of req vecor is 2");
     end
     ack_implies_vld: assert property (@(posedge clk_i) disable iff (~rst_ni) |ack_o |-> vld_o)              else $fatal (1,"an asserted ack signal implies that vld_o must be asserted, too");
