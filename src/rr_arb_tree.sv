34d8ed90e03209501fa64da5cb30068846caaeca
Stefan Mach <smach@iis.ee.ethz.ch>
2019-04-26 16:02:34 +0200

    :sparkles: Add AXI optimization to `rr_arb_tree`

--- src/rr_arb_tree.sv
+++ src/rr_arb_tree.sv
@@ -25,7 +25,10 @@
 // rotating priorities that are operating in lock-step. If static priority arbitration
 // is needed, just connect `rr_i` to '0.
 //
-// The req/gnt signals are compliant with the AXI style vld/rdy handshake.
+// If `AxiVldRdy` is set, the req/gnt signals are compliant with the AXI style vld/rdy
+// handshake. Namely, upstream vld (req) must not depend on rdy (gnt), as it can be deasserted
+// again even though vld is asserted. Enabling `AxiVldRdy` leads to a reduction of arbiter
+// delay and area.
 //
 
 module rr_arb_tree #(
@@ -33,6 +36,7 @@ module rr_arb_tree #(
   parameter int unsigned DataWidth  = 32,
   parameter type         DataType   = logic [DataWidth-1:0],
   parameter bit          ExtPrio    = 1'b0, // set to 1'b1 to enable
+  parameter bit          AxiVldRdy  = 1'b0, // treat req/gnt as vld/rdy
   parameter bit          LockIn     = 1'b0  // set to 1'b1 to enable
 ) (
   input  logic                             clk_i,
@@ -97,7 +101,7 @@ module rr_arb_tree #(
           end
         end
       end else begin : gen_no_lock
-        assign req_d      = req_i;
+        assign req_d = req_i;
       end
 
       assign rr_d       = (gnt_i && req_o) ? ((rr_q == NumLevels'(NumIn-1)) ? '0 : rr_q + 1'b1) : rr_q;
@@ -140,15 +144,15 @@ module rr_arb_tree #(
 
             assign index_nodes[idx0] = NumLevels'(sel);
             assign data_nodes[idx0]  = (sel) ? data_i[l*2+1] : data_i[l*2];
-            assign gnt_o[l*2]        = gnt_nodes[idx0] & req_d[l*2]   & ~sel;
-            assign gnt_o[l*2+1]      = gnt_nodes[idx0] & req_d[l*2+1] & sel;
+            assign gnt_o[l*2]        = gnt_nodes[idx0] & (AxiVldRdy | req_d[l*2])   & ~sel;
+            assign gnt_o[l*2+1]      = gnt_nodes[idx0] & (AxiVldRdy | req_d[l*2+1]) & sel;
           end
           // if only the first index is still in the vector...
           if (unsigned'(l) * 2 == NumIn-1) begin
             assign req_nodes[idx0]   = req_d[l*2];
             assign index_nodes[idx0] = '0;// always zero in this case
             assign data_nodes[idx0]  = data_i[l*2];
-            assign gnt_o[l*2]        = gnt_nodes[idx0] & req_d[l*2];
+            assign gnt_o[l*2]        = gnt_nodes[idx0] & (AxiVldRdy | req_d[l*2]);
           end
           // if index is out of range, fill up with zeros (will get pruned)
           if (unsigned'(l) * 2 > NumIn-1) begin
@@ -192,8 +196,12 @@ module rr_arb_tree #(
         else $fatal (1, "Grant out implies grant in.");
 
     gnt1 : assert property(
-      @(posedge clk_i) disable iff (!rst_ni) gnt_i |-> |gnt_o)
-        else $fatal (1, "Grant in implies grant out.");
+      @(posedge clk_i) disable iff (!rst_ni) req_o |-> gnt_i |-> |gnt_o)
+        else $fatal (1, "Req out and grant in implies grant out.");
+
+    gnt_idx : assert property(
+      @(posedge clk_i) disable iff (!rst_ni) req_o |->  gnt_i |-> gnt_o[idx_o])
+        else $fatal (1, "Idx_o / gnt_o do not match.");
 
     req0 : assert property(
       @(posedge clk_i) disable iff (!rst_ni) |req_i |-> req_o)
@@ -202,10 +210,6 @@ module rr_arb_tree #(
     req1 : assert property(
       @(posedge clk_i) disable iff (!rst_ni) |req_o |-> req_i)
         else $fatal (1, "Req out implies req in.");
-
-    gnt_idx : assert property(
-      @(posedge clk_i) disable iff (!rst_ni) gnt_i |-> gnt_o[idx_o])
-        else $fatal (1, "Idx_o / gnt_o do not match.");
 
     lock: assert property(
       @(posedge clk_i) disable iff (!rst_ni) LockIn |-> req_o && !gnt_i |=> idx_o == $past(idx_o))
