7590ab68eeec7bf764e9c90f7aa795be0428f12c
Iztok Jeras <iztok.jeras@gmail.com>
2020-07-11 19:08:45 +0200

    fixed type casting in a copy/paste typo, should not change functionality, might avoid a warning

--- src/rr_arb_tree.sv
+++ src/rr_arb_tree.sv
@@ -281,7 +281,7 @@ module rr_arb_tree #(
           // if index is out of range, fill up with zeros (will get pruned)
           if (unsigned'(l) * 2 > NumIn-1) begin
             assign req_nodes[idx0]   = 1'b0;
-            assign index_nodes[idx0] = DataType'('0);
+            assign index_nodes[idx0] = idx_t'('0);
             assign data_nodes[idx0]  = DataType'('0);
           end
         //////////////////////////////////////////////////////////////
