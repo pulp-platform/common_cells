a8250734546963b0ac73b5c583d2e3179c55e400
hermanschmit <hermanschmit@users.noreply.github.com>
2021-02-16 13:54:55 -0800

    Update src/id_queue.sv
    Co-authored-by: Florian Zaruba <zarubaf@iis.ee.ethz.ch>

--- src/id_queue.sv
+++ src/id_queue.sv
@@ -268,7 +268,7 @@ module id_queue #(
 `ifndef VERILATOR
     initial begin: validate_params
         assert (ID_WIDTH >= 1)
-            else $fatal(1,"The ID must at least be one bit wide!");
+            else $fatal(1, "The ID must at least be one bit wide!");
         assert (CAPACITY >= 1)
             else $fatal(1,"The queue must have capacity of at least one entry!");
     end
