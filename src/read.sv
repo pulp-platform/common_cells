d4a2b2d0c7d8a2f157984ed2f7b27162c3451f45
davide schiavone <davide@openhwgroup.org>
2022-11-09 10:17:50 +0100

    update read module parameters

--- src/read.sv
+++ src/read.sv
@@ -6,9 +6,12 @@
 // Date: 07.11.2022
 // Description: Dummy circuit to assign a signal, prevent signal being removed after non-ungroupped synthesis compilation
 
-module read (
-    input  logic d_i,
-    output logic d_o
+module read #(
+    parameter int unsigned Width = 1,
+    parameter type T = logic [Width-1:0]
+) (
+    input  T d_i,
+    output T d_o
 );
 
   assign d_o = d_i;
