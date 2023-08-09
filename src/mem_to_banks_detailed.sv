e1b5fa2df1abc9fb79936bcab7d4ac0547376c6f
Eugene Feinberg <uge@persimmons.ai>
2023-08-09 05:27:43 -0700

    Change NumBanks default to 1 to avoid divide by zero

--- src/mem_to_banks_detailed.sv
+++ src/mem_to_banks_detailed.sv
@@ -22,7 +22,7 @@ module mem_to_banks_detailed #(
   /// Response sideband width.
   parameter int unsigned RUserWidth = 32'd0,
   /// Number of banks at output, must evenly divide `DataWidth`.
-  parameter int unsigned NumBanks  = 32'd0,
+  parameter int unsigned NumBanks  = 32'd1,
   /// Remove transactions that have zero strobe
   parameter bit          HideStrb  = 1'b0,
   /// Number of outstanding transactions
