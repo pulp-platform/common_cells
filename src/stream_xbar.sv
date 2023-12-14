145e3bed486551feacb074fc1438a9afdefa2a40
Luca Colagrande <luca.colagrande3@gmail.com>
2023-12-14 12:16:15 +0100

    stream_xbar: Fix non-existing output assertion (#204)

--- src/stream_xbar.sv
+++ src/stream_xbar.sv
@@ -171,7 +171,7 @@ module stream_xbar #(
   `endif
   for (genvar i = 0; unsigned'(i) < NumInp; i++) begin : gen_sel_assertions
     assert property (@(posedge clk_i) disable iff (~rst_ni)
-        (valid_i[i] |-> sel_i[i] < sel_oup_t'(NumOut))) else
+        (valid_i[i] |-> sel_i[i] < NumOut)) else
         $fatal(1, "Non-existing output is selected!");
   end
 
