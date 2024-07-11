f8c739be0b88bb065a75715eb60dc1daf36bbb98
Michael Rogenmoser <michael@rogenmoser.us>
2024-07-11 11:54:42 +0200

    `mem_to_banks_detailed`: Ensure no spurious response after full dead write

--- src/mem_to_banks_detailed.sv
+++ src/mem_to_banks_detailed.sv
@@ -109,8 +109,13 @@ module mem_to_banks_detailed #(
                         resp_valid, resp_ready;
   req_t [NumBanks-1:0]  bank_req,
                         bank_oup;
-  logic [NumBanks-1:0]  bank_req_internal, bank_gnt_internal, zero_strobe, dead_response;
-  logic                 dead_write_fifo_full;
+  logic [NumBanks-1:0]  bank_req_internal,
+                        bank_gnt_internal,
+                        zero_strobe,
+                        dead_response,
+                        dead_response_unmasked;
+  logic                 dead_write_fifo_full,
+                        dead_write_fifo_empty;
 
   function automatic addr_t align_addr(input addr_t addr);
     return (addr >> $clog2(DataBytes)) << $clog2(DataBytes);
@@ -173,16 +178,19 @@ module mem_to_banks_detailed #(
       .flush_i    ( 1'b0                    ),
       .testmode_i ( 1'b0                    ),
       .full_o     ( dead_write_fifo_full    ),
-      .empty_o    (),
+      .empty_o    ( dead_write_fifo_empty   ),
       .usage_o    (),
       .data_i     ( bank_we_o & zero_strobe ),
       .push_i     ( req_i & gnt_o           ),
-      .data_o     ( dead_response           ),
+      .data_o     ( dead_response_unmasked  ),
       .pop_i      ( rvalid_o                )
     );
+    assign dead_response = dead_response_unmasked & {NumBanks{~dead_write_fifo_empty}};
   end else begin : gen_no_dead_write_fifo
+    assign dead_response_unmasked = '0;
     assign dead_response = '0;
     assign dead_write_fifo_full = 1'b0;
+    assign dead_write_fifo_empty = 1'b1;
   end
 
   // Handle responses.
