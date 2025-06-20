67959c4642d51d4adaa8128cb6a23523c5e3d40a
Max Wipfli <mwipfli@ethz.ch>
2025-06-20 17:54:51 +0200

    lzc: Add Verilator pragmas to split tree node signals
    This enables Verilator optimizations as it now understands there are no
    combinatorial loops. For the Cheshire SoC, this can reduce simulation
    time by around 2.5%.

--- src/lzc.sv
+++ src/lzc.sv
@@ -46,8 +46,8 @@ module lzc #(
   `endif
 
     logic [WIDTH-1:0][NumLevels-1:0] index_lut;
-    logic [2**NumLevels-1:0] sel_nodes;
-    logic [2**NumLevels-1:0][NumLevels-1:0] index_nodes;
+    logic [2**NumLevels-1:0] sel_nodes                  /* verilator split_var */;
+    logic [2**NumLevels-1:0][NumLevels-1:0] index_nodes /* verilator split_var */;
 
     logic [WIDTH-1:0] in_tmp;
 
