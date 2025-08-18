b20ad7aba2080b3bb705680e0f70f1fc180ce290
Max Wipfli <mwipfli@ethz.ch>
2025-08-18 16:22:35 +0200

    treewide: Optimizations for faster Verilator simulation (#259)
    * rr_arb_tree: Add Verilator pragmas to split tree node signals
    
    This enables Verilator optimizations as it now understands there are no
    combinatorial loops. For the Cheshire SoC, this can reduce simulation
    time by around 3%.
    
    * lzc: Add Verilator pragmas to split tree node signals
    
    This enables Verilator optimizations as it now understands there are no
    combinatorial loops. For the Cheshire SoC, this can reduce simulation
    time by around 2.5%.
    
    * lzc: Optimize reversing of input vector for Verilator speedup
    
    In case the vector is not flipped, using a direct assignment can lead to
    an approximately 2% decrease in total system simulation time (measured
    using the Cheshire SoC).
    
    * cb_filter: Logic simplification for Verilator speed-up
    
    This decreases total simulation time of the Cheshire SoC by around 1.8%.

--- src/lzc.sv
+++ src/lzc.sv
@@ -46,16 +46,21 @@ module lzc #(
   `endif
 
     logic [WIDTH-1:0][NumLevels-1:0] index_lut;
-    logic [2**NumLevels-1:0] sel_nodes;
-    logic [2**NumLevels-1:0][NumLevels-1:0] index_nodes;
+    logic [2**NumLevels-1:0] sel_nodes                  /* verilator split_var */;
+    logic [2**NumLevels-1:0][NumLevels-1:0] index_nodes /* verilator split_var */;
 
     logic [WIDTH-1:0] in_tmp;
 
-    // reverse vector if required
-    always_comb begin : flip_vector
-      for (int unsigned i = 0; i < WIDTH; i++) begin
-        in_tmp[i] = (MODE) ? in_i[WIDTH-1-i] : in_i[i];
+    if (MODE) begin : g_flip
+      // Mode 1 (leading zero): flip input vector
+      always_comb begin : flip_vector
+        for (int unsigned i = 0; i < WIDTH; i++) begin
+          in_tmp[i] = in_i[WIDTH-1-i];
+        end
       end
+    end else begin
+      // Mode 0 (trailing zero)
+      assign in_tmp = in_i;
     end
 
     for (genvar j = 0; unsigned'(j) < WIDTH; j++) begin : g_index_lut
