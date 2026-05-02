742528367a0db775998962093f47351b1600837e
Philippe Sauter <phsauter@iis.ee.ethz.ch>
2026-05-03 00:24:19 +0200

    restore serial deglitch and majority vote filter

---
+++ src/cc_majority_vote_filter.sv
@@ -0,0 +1,61 @@
+// Copyright 2026 ETH Zurich and University of Bologna.
+// Copyright and related rights are licensed under the Solderpad Hardware
+// License, Version 0.51 (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+// http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
+// or agreed to in writing, software, hardware and materials distributed under
+// this License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+// Authors:
+// - Philippe Sauter <phsauter@iis.ee.ethz.ch>
+//
+// Description: Majority-Vote Filter
+// Smooth noisy data using a moving window threshold vote.
+// The output q_o is high if at least THRESHOLD of
+// the last WINDOW_LEN samples are '1', otherwise low.
+// New data is shifted in when enabled (en_i).
+// clear_i synchronously resets the history and output to '0.
+
+`include "common_cells/registers.svh"
+`include "common_cells/assertions.svh"
+
+module cc_majority_vote_filter #(
+    parameter int unsigned WINDOW_LEN = 4,
+    parameter int unsigned THRESHOLD  = (WINDOW_LEN / 2) + 1
+)(
+    input  logic clk_i,
+    input  logic rst_ni,
+    input  logic clear_i,
+    input  logic en_i,
+    input  logic d_i,
+    output logic q_o
+);
+    localparam int unsigned CNT_WIDTH = $clog2(WINDOW_LEN+1);
+
+    logic [CNT_WIDTH-1:0]  popcount_o;
+    logic [WINDOW_LEN-1:0] history_d, history_q;
+
+    assign history_d = {history_q[WINDOW_LEN-2:0], d_i};
+    `FFLARNC(history_q, history_d, en_i, clear_i, '0, clk_i, rst_ni)
+
+    cc_popcount #(
+        .INPUT_WIDTH(WINDOW_LEN)
+    ) i_popcount (
+        .data_i(history_q),
+        .popcount_o(popcount_o)
+    );
+
+    assign q_o = (popcount_o >= CNT_WIDTH'(THRESHOLD));
+
+`ifndef COMMON_CELLS_ASSERTS_OFF
+    `ASSERT_INIT(WindowLenGeqZero, WINDOW_LEN >= 2,
+                "cc_majority_vote_filter: WINDOW_LEN must be >= 2")
+    `ASSERT_INIT(ThresholdGeqZero, THRESHOLD >= 1,
+                "cc_majority_vote_filter: THRESHOLD must be >= 1")
+    `ASSERT_INIT(ThresholdLeqWindowLen, THRESHOLD <= WINDOW_LEN,
+                "cc_majority_vote_filter: THRESHOLD must be <= WINDOW_LEN")
+`endif
+
+endmodule
--- src/cc_serial_deglitch.sv
+++ src/cc_serial_deglitch.sv
@@ -1,4 +1,4 @@
-// Copyright 2018 ETH Zurich and University of Bologna.
+// Copyright 2026 ETH Zurich and University of Bologna.
 // Copyright and related rights are licensed under the Solderpad Hardware
 // License, Version 0.51 (the "License"); you may not use this file except in
 // compliance with the License. You may obtain a copy of the License at
@@ -8,43 +8,52 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
-// Author: Florian Zaruba
-// Description: Deglitches a serial line by taking multiple samples until
-//              asserting the output high/low.
+// Authors:
+// - Philippe Sauter <phsauter@iis.ee.ethz.ch>
+//
+// Description: Serial Line Deglitcher
+// Update output only after d_i has remained stable for THRESHOLD cycles.
+// The output q_o changes to the current level of d_i only after
+// d_i has remained stable at that level for at least
+// THRESHOLD consecutive enabled (en_i) clock cycles.
+// clear_i synchronously resets the history and immediately sets q_o to current d_i.
+
+`include "common_cells/registers.svh"
+`include "common_cells/assertions.svh"
 
 module cc_serial_deglitch #(
-    parameter int unsigned SIZE = 4
+    parameter int unsigned THRESHOLD = 4
 )(
-    input  logic clk_i,    // clock
-    input  logic rst_ni,   // asynchronous reset active low
-    input  logic en_i,     // enable
-    input  logic d_i,      // serial data in
-    output logic q_o       // filtered data out
+    input  logic clk_i,
+    input  logic rst_ni,
+    input  logic clear_i,
+    input  logic en_i,
+    input  logic d_i,
+    output logic q_o
 );
-    logic [SIZE-1:0] count_q;
-    logic q;
+    localparam int unsigned CNT_WIDTH = cc_pkg::idx_width(THRESHOLD + 1);
 
-    always_ff @(posedge clk_i or negedge rst_ni) begin
-        if (~rst_ni) begin
-            count_q <= '0;
-            q       <= 1'b0;
-        end else begin
-            if (en_i) begin
-                if (d_i == 1'b1 && count_q != SIZE[SIZE-1:0]) begin
-                    count_q <= count_q + 1;
-                end else if (d_i == 1'b0 && count_q != SIZE[SIZE-1:0]) begin
-                    count_q <= count_q - 1;
-                end
-            end
-        end
-    end
+    logic [CNT_WIDTH-1:0] count_q, count_d;
+    logic                 mismatch, stable_edge;
+    logic                 count_load, count_clear;
+    logic                 q_d;
 
-    // output process
-    always_comb begin
-        if (count_q == SIZE[SIZE-1:0]) begin
-            q_o = 1'b1;
-        end else if (count_q == 0) begin
-            q_o = 1'b0;
-        end
-    end
+    assign mismatch = (d_i != q_o);
+    assign count_d  = count_q + CNT_WIDTH'(1);
+
+    // Update when the mismatch counter reaches THRESHOLD.
+    assign stable_edge = en_i && mismatch && (count_d == CNT_WIDTH'(THRESHOLD));
+    // Clear if signal is not different/stable or when updating the output.
+    assign count_clear = clear_i || (en_i && !mismatch) || stable_edge;
+    assign count_load  = en_i && mismatch && !stable_edge;
+
+    assign q_d = (clear_i || stable_edge) ? d_i : q_o;
+
+    `FFLARNC(count_q, count_d, count_load, count_clear, '0,  clk_i, rst_ni)
+    `FF(q_o, q_d, 1'b0, clk_i, rst_ni)
+
+`ifndef COMMON_CELLS_ASSERTS_OFF
+    `ASSERT_INIT(ThresholdGtZero, THRESHOLD >= 1, "cc_serial_deglitch: THRESHOLD must be >= 1")
+`endif
+
 endmodule
