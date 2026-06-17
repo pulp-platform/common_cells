5824afc875ecabcea7b5f87faed4d594da849e65
Philippe Sauter <phsauter@iis.ee.ethz.ch>
2026-06-17 20:54:27 +0200

    cdc_clearable: Add verification baseline

---
+++ formal/run_cdc_reset_ctrlr_half.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# Copyright 2026 ETH Zurich and University of Bologna.
+#
+# Copyright and related rights are licensed under the Solderpad Hardware
+# License, Version 0.51 (the "License"); you may not use this file except in
+# compliance with the License. You may obtain a copy of the License at
+# http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
+# or agreed to in writing, software, hardware and materials distributed under
+# this License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+#
+# Authors:
+# - Philippe Sauter <phsauter@iis.ee.ethz.ch>
+#
+# Description: Reset-Controller Half Proof Runner
+# Run the reset-controller half SymbiYosys proof from the formal directory.
+
+set -euo pipefail
+
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+: "${SBY:=sby}"
+
+cd "${script_dir}"
+"${SBY}" -f cdc_reset_ctrlr_half.sby
